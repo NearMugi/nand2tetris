8cf1d70d18b9ada97000f738a1fcb26a166d478a
Teppei Kuroda <niamugi@gmail.com>
2020-08-19 21:11:27 +0900

    update mult

--- nand2tetris/projects/04/mult/mult.asm
+++ nand2tetris/projects/04/mult/mult.asm
@@ -7,3 +7,32 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+    @cnt
+    M=0
+    @mult
+    M=0
+(LOOP)
+    @cnt
+    D=M
+    @R1
+    D=D-M
+    @SET
+    D;JGE
+
+    @R0
+    D=M
+    @mult
+    M=M+D
+    @cnt
+    M=M+1
+    @LOOP
+    0;JMP
+
+(SET)
+    @mult
+    D=M
+    @R2
+    M=D
+(END)
+    @END
+    0;JMP
