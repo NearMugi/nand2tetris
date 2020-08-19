39565b394220018d41fa87b20e5211baeecd3222
Teppei Kuroda <niamugi@gmail.com>
2020-08-20 08:45:33 +0900

    update Fill.asm

--- nand2tetris/projects/04/fill/Fill.asm
+++ nand2tetris/projects/04/fill/Fill.asm
@@ -12,3 +12,71 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+(LOOP)
+    // input
+    @KBD
+    D=M
+    @BLACK
+    D;JGT
+    // white
+    @color
+    M=0
+    @DRAW
+    0;JMP
+(BLACK)
+    @color
+    M=-1
+(DRAW)
+    @y
+    M=0
+(DRAWLOOP)
+    @y
+    D=M
+    @8192
+    D=D-A
+    @DRAWEND
+    D;JGE
+
+    @y
+    D=M
+    @SCREEN
+    D=A+D
+    @address
+    M=D-1
+    @x
+    M=0
+
+(DRAWCOL)
+    @x
+    D=M
+    @32
+    D=D-A
+    @DRAWCOLEND
+    D;JGE
+
+    @address
+    M=M+1
+    @color
+    D=M
+    @address
+    A=M
+    M=D
+
+    @x
+    M=M+1
+    @DRAWCOL
+    0;JMP
+(DRAWCOLEND)
+
+    // next Loop
+    @32
+    D=A
+    @y
+    M=M+D
+
+    @DRAWLOOP
+    0;JMP
+(DRAWEND)
+    @LOOP
+    0;JMP
+
